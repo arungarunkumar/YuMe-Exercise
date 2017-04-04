ea6050d31876ac676cacfee9016b7395cea78ead
arunkumar.n <arunkumar.n@krds.fr>
2017-04-05 01:37:18 +0530

    Initial commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,7 @@
 $(document).ready(function()
 {
   $('.toggle').click(function(e) {
+      e.preventDefault();
       //console.log($('.reportWrapper').not('#' + $(this).data('ele')));
       $('.reportWrapper').not('#' + $(this).data('ele')).hide();
 
